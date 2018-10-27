e62e0d06d3245bbd5627b6888b4d2679c154db52
Brajesh Singh <brajesh@cosmiccoders.com>
2018-10-28 01:12:31 +0530

    Cleanup js file

--- assets/js/bpln.js
+++ assets/js/bpln.js
@@ -1,149 +1,124 @@
 /*BuddyPress Live Notification Javascript*/
-jQuery(document).ready(function(){
-	
-    var jq = jQuery;
-	
-	var last_notified = bpln.last_notified;
-	
-	var new_notifications_count = 0;
-	
-	
-	
-    //set interval to 5s 
-	wp.heartbeat.interval( 'fast' );
-	
-	jq(document).on( 'heartbeat-tick.bpln-data', function( event, data ) {
-        
-		if ( data.hasOwnProperty( 'bpln-data' ) ) {
-			var bpln_data = data['bpln-data'] ;
-			
-			update_last_notified( bpln_data.last_notified );
-			
-			var messages = bpln_data.messages;
-			
-			if( messages == undefined || messages.length == 0 )
-				return ;
-			
-			for( var i =0; i< messages.length; i++ ) {
-			   bpln.notify(messages[i]);
-			  
-			}
-			//fire custom event bpln:new_notifications
-			fire_on_new_notifications_recieved(messages.length, messages );
+jQuery(document).ready(function ($) {
+
+    var _lastNotified = bpln.last_notified;
+    var _newNotificationsCount = 0;
+
+    //set interval to 5s
+    wp.heartbeat.interval('fast');
+
+    $(document).on('heartbeat-tick.bpln-data', function (event, data) {
+
+        if (data.hasOwnProperty('bpln-data')) {
+            var bplnData = data['bpln-data'];
+
+            updateLastNotified(bplnData.last_notified);
+
+            var messages = bplnData.messages;
+
+            if (messages === undefined || messages.length === 0) {
+                return;
+            }
+
+            for (var i = 0; i < messages.length; i++) {
+                bpln.notify(messages[i]);
+            }
+
+            // fire custom event bpln:new_notifications.
+            emitNewNotificationsRecieved(messages.length, messages);
         }
     });
-	
-	jq(document).on( 'heartbeat-send', function( e, data ) {
-			data['bpln-data'] = {last_notified: get_last_notified()};
-	});
 
-/**
- * Overwrite bpln.notify to use your own notification like sweetalert or growl
- * 
- * @param {type} message
- * @returns {undefined}
- */	
-function notify( message ) {
-	
-	if( jq.achtung != undefined )
-		jq.achtung({message: message, timeout: bpln.timeout});//show for 10 seconds	
-}
+    $(document).on('heartbeat-send', function (e, data) {
+        data['bpln-data'] = {last_notified: getLastNotified()};
+    });
+
+    /**
+     * Overwrite bpln.notify to use your own notification like sweetalert or growl
+     *
+     * @param {type} message
+     * @returns {undefined}
+     */
+    function notify(message) {
+
+        if ($.achtung === undefined) {
+            return;
+        }
+        $.achtung({
+            message: message,
+            timeout: bpln.timeout
+        }); // show for 10 seconds.
+    }
 
 
 //a theme author can override the bpln.notify and that will be used instead of the bundled notification system
-bpln.notify = notify;
+    bpln.notify = notify;
+    bpln.get_count = function get_count() {
+        return _newNotificationsCount;
+    };
 
-function get_count() {
-	
-	return new_notifications_count;
-	
-}
+    $(document).on('bpln:new_notifications', function (evt, data) {
 
-bpln.get_count = get_count;//seems meaningless to me now but let us keep it for now
+        if (data.count && data.count > 0) {
+            updateCountText($('#ab-pending-notifications'), data.count);
 
-//see what I am doing here, well don't follow me as I am breaking my own rules in the below code but still you can get an idea from the event handling 
+            var myAccountNotificationMenu = $('#wp-admin-bar-my-account-notifications > a span');
+            // if the count menu does not exist.
+            if (!myAccountNotificationMenu.length) {
+                if ($('#wp-admin-bar-my-account-notifications').length) {
+                    $('#wp-admin-bar-my-account-notifications > a').append(' <span class="count">' + data.count + " </span>");
+                    $('#wp-admin-bar-my-account-notifications-unread a').append(' <span class="count">' + data.count + " </span>");
+                }
+            } else {
+                updateCountText(myAccountNotificationMenu, data.count);
+                updateCountText($('#wp-admin-bar-my-account-notifications-unread span'), data.count);
+            }
 
-jq( document ).on('bpln:new_notifications', function(evt, data ){
-		
-	if( data.count && data.count>0 ){
-	
-		update_count_text( jq('#ab-pending-notifications'), data.count );
-		
-		var my_act_notification_menu = jq('#wp-admin-bar-my-account-notifications > a span');
-		//if the count menu does not exis
-		if(  ! my_act_notification_menu.get(0 ) ) {
-		
-			
-			if( jq('#wp-admin-bar-my-account-notifications').get(0) ) { 
-					jq('#wp-admin-bar-my-account-notifications > a').append(' <span class="count">'+data.count+" </span>");
-					jq('#wp-admin-bar-my-account-notifications-unread a').append(' <span class="count">'+data.count+" </span>");
-			}
-		}else{
-			
-			update_count_text( my_act_notification_menu, data.count );
-			update_count_text( jq('#wp-admin-bar-my-account-notifications-unread span'), data.count );
-			
-		}
-		
-		var list_parent = jq('#wp-admin-bar-bp-notifications-default');
-		
-		if( list_parent.get(0) ) {
-			
-			list_parent.append("<li>"+data.messages.join("</li><li>") + "</li>"  );
-			
-			
-		}
-		
-		
-	}
-	
-});
+            var $listParent = $('#wp-admin-bar-bp-notifications-default');
 
+            if ($listParent.length) {
+                $listParent.append("<li>" + data.messages.join("</li><li>") + "</li>");
+            }
+        }
+    });
 
+// private functions.
+    /**
+     * Get last notified time
+     *
+     * @returns string
+     */
+    function getLastNotified() {
+        // last notified is accessible in this scope but not outside.
+        return _lastNotified;
+    }
 
-//private functions 
-/**
- * Get last notified time
- * 
- * @returns string
- */
-function get_last_notified() {
-	//last notified is accessible in this scope but not outside
-	return last_notified;
-}
+    /**
+     * Set last notified time
+     *
+     * @param time String datetime
+     * @returns null
+     */
+    function updateLastNotified(time) {
+        _lastNotified = time;
+    }
 
-/**
- * Set last notified time
- * 
- * @param time String datetime
- * @returns null
- */	
-function update_last_notified( time ) {
+    function emitNewNotificationsRecieved(count, messages) {
+        $(document).trigger('bpln:new_notifications', [{count: count, messages: messages}]);
+    }
 
-	last_notified = time;
-}
+    function updateCountText(elements, count) {
+        // don't do anything if the element does not exist or the count is zero.
+        if (!elements.length || !count) {
+            return;
+        }
 
-function fire_on_new_notifications_recieved(count, messages) {
-	
-	jq( document ).trigger( "bpln:new_notifications", [{count: count, messages: messages}] );
-}
+        elements.each(function () {
+            var $element = $(this);
+            var currentCount = parseInt($element.text());
+            currentCount = currentCount + parseInt(count) - 0;
+            $element.text('' + currentCount);
+        });
+    }
 
-function update_count_text( elements, count) {
-	//don't do anything if the element does not exist or the count is zero
-	
-	if( ! elements.get(0) || ! count  )
-		return;
-	
-	elements.each( function() {
-		var element = jq(this);
-		var current_count = parseInt( element.text() );
-		
-		current_count = current_count + parseInt(count) - 0;
-	
-		element.text( '' + current_count );
-	});
-	
-	
-}
-
-});//end of jq(document).ready()
+});//end of $(document).ready()
